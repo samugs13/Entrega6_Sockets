252802b3cb1ac78fa97fd9a68e1eccdd86bd042f
sambita13 <garciasanchezsamu@gmail.com>
2021-04-12 13:30:51 +0200

    terminada

--- main.js
+++ main.js
@@ -4,48 +4,63 @@ const quiz = require("./cmds_quiz.js");
 const favs = require("./cmds_favs.js");
 const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: "> "
-});
-rl.log = (msg) => console.log(msg);  // Add log to rl interface
-rl.questionP = function (string) {   // Add questionP to rl interface
-  return new Promise ( (resolve) => {
-    this.question(`  ${string}: `, (answer) => resolve(answer.trim()))
-  })
-};
+let net = require('net');
+let port = 8080;
 
-rl.prompt();
+let server = net.createServer((socket)=>{
 
-rl.on('line', async (line) => {
-  try{
-    let cmd = line.trim()
+  console.log("Se ha conectado un nuevo cliente");
 
-    if      ('' ===cmd)   {}
-    else if ('h' ===cmd)  { user.help(rl);}
+  socket
+    .on('end', ()=> {rl.close();})
+    .on('error', ()=> {rl.close();})
 
-    else if (['lu', 'ul', 'u'].includes(cmd)) { await user.list(rl);}
-    else if (['cu', 'uc'].includes(cmd))      { await user.create(rl);}
-    else if (['ru', 'ur', 'r'].includes(cmd)) { await user.read(rl);}
-    else if (['uu'].includes(cmd))            { await user.update(rl);}
-    else if (['du', 'ud'].includes(cmd))      { await user.delete(rl);}
-
-    else if (['lq', 'ql', 'q'].includes(cmd)) { await quiz.list(rl);}
-    else if (['cq', 'qc'].includes(cmd))      { await quiz.create(rl);}
-    else if (['tq', 'qt', 't'].includes(cmd)) { await quiz.test(rl);}
-    else if (['uq', 'qu'].includes(cmd))      { await quiz.update(rl);}
-    else if (['dq', 'qd'].includes(cmd))      { await quiz.delete(rl);}
-
-    else if (['lf', 'fl', 'f'].includes(cmd)) { await favs.list(rl);}
-    else if (['cf', 'fc'].includes(cmd))      { await favs.create(rl);}
-    else if (['df', 'fd'].includes(cmd))      { await favs.delete(rl);}
-
-    else if ('e'===cmd)  { rl.log('Bye!'); process.exit(0);}
-    else                 {  rl.log('UNSUPPORTED COMMAND!');
-                            user.help(rl);
-                         };
-    } catch (err) { rl.log(`  ${err}`);}
-    finally       { rl.prompt(); }
+  const rl = readline.createInterface({
+    input: socket,
+    output: socket,
+    prompt: "> "
   });
+  rl.log = (msg) => socket.write(msg + "\n");  // Add log to rl interface
+  rl.questionP = function (string) {   // Add questionP to rl interface
+    return new Promise ( (resolve) => {
+      this.question(`  ${string}: `, (answer) => resolve(answer.trim()))
+    })
+  };
 
+  rl.prompt();
+
+  rl.on('line', async (line) => {
+    try{
+      let cmd = line.trim()
+
+      if      ('' ===cmd)   {}
+      else if ('h' ===cmd)  { user.help(rl);}
+
+      else if (['lu', 'ul', 'u'].includes(cmd)) { await user.list(rl);}
+      else if (['cu', 'uc'].includes(cmd))      { await user.create(rl);}
+      else if (['ru', 'ur', 'r'].includes(cmd)) { await user.read(rl);}
+      else if (['uu'].includes(cmd))            { await user.update(rl);}
+      else if (['du', 'ud'].includes(cmd))      { await user.delete(rl);}
+
+      else if (['lq', 'ql', 'q'].includes(cmd)) { await quiz.list(rl);}
+      else if (['cq', 'qc'].includes(cmd))      { await quiz.create(rl);}
+      else if (['tq', 'qt', 't'].includes(cmd)) { await quiz.test(rl);}
+      else if (['uq', 'qu'].includes(cmd))      { await quiz.update(rl);}
+      else if (['dq', 'qd'].includes(cmd))      { await quiz.delete(rl);}
+
+      else if (['lf', 'fl', 'f'].includes(cmd)) { await favs.list(rl);}
+      else if (['cf', 'fc'].includes(cmd))      { await favs.create(rl);}
+      else if (['df', 'fd'].includes(cmd))      { await favs.delete(rl);}
+
+      else if ('e'===cmd)  { rl.log('Bye!'); 
+                              socket.end();}
+      else                 {  rl.log('UNSUPPORTED COMMAND!');
+                              user.help(rl);
+                          };
+      } catch (err) { rl.log(`  ${err}`);}
+      finally       { rl.prompt(); }
+    });
+
+});
+
+server.listen(port, ()=> console.log("SERVIDOR ESCUCHANDO"));
